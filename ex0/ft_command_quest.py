bdca8de376c7d9a11b8b007067b217f3c8513bfc
Meryem Idbellaali <meridbel@c4r7p7.1337.ma>
2026-01-28 11:53:27 +0100

    finished first exercice

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -1,6 +1,17 @@
 import sys
-lent = len(sys.argv)
-listpy = [lent - 1]
-try:
-    for x in range(1, lent)
-    x = int(x)
+
+print("=== Command Quest ===")
+program_name = sys.argv[0]
+if len(sys.argv) == 1:
+    print("No arguments provided!")
+    print(f"Program name: {program_name}")
+    print(f"Total arguments: {len(sys.argv)}")
+
+if len(sys.argv) > 1:
+    print(f"Program name: {program_name}")
+    print(f"Arguments received: {len(sys.argv) - 1}")
+    i = 1
+    for x in sys.argv[1:]:
+        print(f"Argument {i}: {x}")
+        i = i + 1
+    print(f"Total arguments: {len(sys.argv)}")
